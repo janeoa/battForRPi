81e52d02d7dcff61eff99c547fc90a6c12d43f0f
janeoa <asetmalik@gmail.com>
2018-06-29 16:17:33 +0600

    ggdhfghd

--- testBattery.py
+++ testBattery.py
@@ -5,3 +5,4 @@ batt = Battery()
 while True:
     batt.setBattery('3.7','4.2','5.0')
     toOut = '{0:>4}  {1:>2.2}V {2:>3}%'.format(str(batt.getRaw()),float(batt.getVoltage()),str(batt.getBattery()))
+    print(toOut)
