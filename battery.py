accabfc8ccae657e4716c578df3ffadf0b280ce0
janeoa <asetmalik@gmail.com>
2018-06-29 15:28:39 +0600

    getVoltage is fixed

--- battery.py
+++ battery.py
@@ -4,11 +4,10 @@ class Battery(object):
     
     def __init__(self):
         self.serialport = serial.Serial("/dev/serial0", 9600)
-
-    _vcc = 5.0
-    _low = 3.0
-    _high = 4.2
-    _dV = _high-_low
+        self._vcc = 5.0
+        self._low = 3.0
+        self._high = 4.2
+        self._dV = self._high-self._low
 
     def getRaw(self):
         raw = serialport.readline()
@@ -16,7 +15,7 @@ class Battery(object):
 
     def getVoltage(self):
         raw  = self.serialport.readline()
-        voltage = float(float(raw)*_vcc/1023.0)
+        voltage = float(float(raw)*self._vcc/1023.0)
         return(voltage)
 
     def getBattery(self):
