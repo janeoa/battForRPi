eedf866510c9bb34f6c7a7bfaf007f69577d4442
janeoa <asetmalik@gmail.com>
2018-06-29 16:12:36 +0600

    i see now

--- testBattery.py
+++ testBattery.py
@@ -4,6 +4,4 @@ batt = Battery()
 
 while True:
     batt.setBattery('3.7','4.2','5.0')
-    #toOut = '{0:>4}  {1:>2.2}V {2:>3}%'.format(str(batt.getRaw),float(batt.getVoltage),str(batt.getBattery))
-    print(str(batt.getRaw))
-    
+    toOut = '{0:>4}  {1:>2.2}V {2:>3}%'.format(str(batt.getRaw()),float(batt.getVoltage()),str(batt.getBattery()))
