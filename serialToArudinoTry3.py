ed61640eac2e06d9532f28c749b72ba5c448cd5e
janeoa <asetmalik@gmail.com>
2018-06-29 15:05:45 +0600

    added methods

--- serialToArudinoTry3.py
+++ serialToArudinoTry3.py
@@ -1,14 +1,23 @@
 class Battery:
     import serial
-    from gpiozero import LED
-
-    resetPin = LED(18)
-    resetPin.off()
-    resetPin.on()
     serialport = serial.Serial("/dev/serial0", 9600)
 
-    def getRaw():
+    _vcc = 5.0
+    _low = 3.0
+    _high = 4.2
+    _dV = _high-_low
 
-    while True:
-        command = serialport.readline()
-        print str(command)
+    def getRaw():
+        raw = serialport.readline()
+        return str(raw)
+
+    def getVoltage():
+        raw  = serialport.readline()
+        voltage = float(float(raw)*_vcc/1023.0)
+        return(voltage)
+
+    def getBattery():
+        voltage = getVoltage()
+        percentage = (voltage-_low)*100.0/_dV
+        return percentage
+
